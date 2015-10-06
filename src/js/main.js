ed2286a34f85b0c07f563c774b0c4bbedede0cb0
Shay <shayhall@nccumc.org>
2015-10-05 20:24:01 -0400

    Create link to tabs to remove active class

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 jQuery.ajax('../apis/github/users/octocat.json').then(function(data){
  console.log(arguments);
 });
+
+
+
+$(".tabs").click(function(){
+    addTabs();
+});
+
+
+function addTabs() {
+  $(".tabs").removeClass("active");
+};
